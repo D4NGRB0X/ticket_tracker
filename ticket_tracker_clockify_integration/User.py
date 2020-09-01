93d29948a61b116f8d55046e88c35ef18e9ad41b
Thomas <thomas.meche@gmail.com>
2020-09-01 09:45:38 -0600

    - Created While loop for multiple time entries  - Began sketching user interface  - Renamed config to endpoint_config  - set quit as break for client or ticket    - need to create a way to circle back if call is abandoned

--- ticket_tracker_clockify_integration/User.py
+++ ticket_tracker_clockify_integration/User.py
@@ -1,19 +1,14 @@
-import config
+import endpoint_config
 import requests
 
 
 class User:
     def __init__(self):
-        self.endpoint = requests.get(config.default_endpoint + "user", headers=config.header).json()
+        self.endpoint = requests.get(endpoint_config.default_endpoint + "user", headers=endpoint_config.header).json()
         self.name = self.endpoint['name']
         self.workspace_id = self.endpoint["activeWorkspace"]
         self.user_id = self.endpoint["id"]
         self.email = self.endpoint["email"]
         self.info = [self.name, self.email, self.user_id, self.workspace_id]
-        self.workspaces = requests.get(config.default_endpoint + "workspaces", headers=config.header).json()
+        self.workspaces = requests.get(endpoint_config.default_endpoint + "workspaces", headers=endpoint_config.header).json()
 
-    def start_timer(self):
-        pass
-
-    def end_timer(self):
-        pass
--- ticket_tracker_clockify_integration/main.py
+++ ticket_tracker_clockify_integration/main.py
@@ -1,34 +1,41 @@
 import requests
-import pprint
-import config
+import endpoint_config
 from User import User
 from datetime import datetime, timezone
-import time
 
+user = User()
+workspaceId = user.workspaces[0]["id"]
+projects_endpoint = endpoint_config.default_endpoint + f"workspaces/{workspaceId}/projects"
 
-thomas = User()
-workspaceId = thomas.workspaces[0]["id"]
-projects_endpoint = config.default_endpoint + f"workspaces/{workspaceId}/projects"
-
-projects = {client["clientName"]: client['id'] for client in requests.get(projects_endpoint, headers=config.header).json()}
-
-pprint.pprint(projects)
-client = input("Please select a client:")
-start_timer = datetime.now(tz=timezone.utc).isoformat().replace("+00:00", "Z")
-description = input("Ticket Number:")
-time.sleep(10)
-stop_timer = datetime.now(tz=timezone.utc).isoformat().replace("+00:00", "Z")
-
-payload = {
-  "start": start_timer,
-  "description": description,
-  "projectId": projects[client],
-  "end": stop_timer,
+projects = {
+    client["clientName"]: client['id'] for client in requests.get(
+        projects_endpoint, headers=endpoint_config.header
+    ).json()
 }
 
-time_entry_endpoint = config.default_endpoint + f"workspaces/{workspaceId}/time-entries"
+while True:
+    ##### ON CLIENT SELECTION CLICK #####
+    print(list(projects))  # turn this into buttons for UI
+    client = input("Please select a client: ")  # this will be accomplished by buttons in UI
 
-add_time_entry = requests.post(time_entry_endpoint, headers=config.header, json=payload)
+    if client.upper() == "QUIT":
+        break
 
+    start_timer = datetime.now(tz=timezone.utc).isoformat().replace("+00:00", "Z")
 
-pprint.pprint(add_time_entry)
+    ##### ONCE SELECTION MADE NEW PAGE #####
+    description = input("Ticket Number: ")
+
+    if description.upper() == "QUIT":
+        break
+
+    ###### ON SUBMIT #####
+    stop_timer = datetime.now(tz=timezone.utc).isoformat().replace("+00:00", "Z")
+    payload = {
+        "start": start_timer,
+        "description": description,
+        "projectId": projects[client],
+        "end": stop_timer,
+    }
+    time_entry_endpoint = endpoint_config.default_endpoint + f"workspaces/{workspaceId}/time-entries"
+    add_time_entry = requests.post(time_entry_endpoint, headers=endpoint_config.header, json=payload)
